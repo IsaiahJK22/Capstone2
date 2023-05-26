d109c40c5fc3bada84a3269d4c4ef65de3189162
IsaiahJK22 <130613608+IsaiahJK22@users.noreply.github.com>
2023-05-25 22:49:04 -0700

    Another Update

--- park.js
+++ park.js
@@ -25,25 +25,42 @@ nationalParksArray.forEach(park => {
 // Function to create a Bootstrap card for a park
 function createParkCard(park) {
     const card = document.createElement('div');
-    card.className = 'card col-md-4';
+    card.className = 'card col-md-4 flip-card';
 
-    const cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
+    const cardInner = document.createElement('div');
+    cardInner.className = 'flip-card-inner';
+    
+    const cardFront = document.createElement('div');
+    cardFront.className = 'card-body flip-card-front';
 
     const cardTitle = document.createElement('h5');
     cardTitle.className = 'card-title';
     cardTitle.textContent = park.LocationName;
 
+    const cardImg = document.createElement('img');
+    cardImg.src = park.img;
+    cardImg.className = 'card-img-top';
+
+    cardFront.appendChild(cardImg);
+    cardFront.appendChild(cardTitle);
+
+    const cardBack = document.createElement('div');
+    cardBack.className = 'card-body flip-card-back';
+
     const cardText = document.createElement('p');
     cardText.className = 'card-text';
     cardText.textContent = `${park.Address}, ${park.City}, ${park.State}, ${park.ZipCode}`;
 
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardText);
-    card.appendChild(cardBody);
+    cardBack.appendChild(cardText);
+
+    cardInner.appendChild(cardFront);
+    cardInner.appendChild(cardBack);
+
+    card.appendChild(cardInner);
 
     return card;
 }
+
 
 // Function to filter parks by location and type
 function filterParks() {
